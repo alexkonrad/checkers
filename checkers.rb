4c32638e3dc60756edd84188f12dfbbdb6d9341f
App Academy Student <student@appacademy.io>
2014-01-09 13:22:16 -0500

    Complete perform_slide and perform_jump methods for Piece class.

--- checkers.rb
+++ checkers.rb
@@ -1,56 +1,71 @@
 class Piece
-  attr_accessor :position
+  attr_reader :position, :color
 
-  def initialize(initial_position, last_row)
+  def initialize(initial_position, color)
+    @position, @color = initial_position, color
+
+    @last_row = color == :white ? 7 : 0
     @state = :pawn
-    @position = initial_position
-    @last_row = last_row
   end
 
-  def perform_slide(to)
-    # only kings can move forward and backward,
-    # but all pieces can only slide one space
-    return false unless moves_forward?(to) || king?
-    return false unless moves_one_space?(to)
+  def perform_slide(square)
 
-    # set the new position, check if the piece has def reached
-    # the opposite side of the board, and promote the piece to
-    # king if so
-    @position = to
+    return false unless moves_forward_to?(square) || king?
+    return false unless adjacent_to?(square)
+
+    @position = square
     promote if reached_opposite_side?
 
     true
   end
 
-  def perform_jump
+  def perform_jump(other_piece)
+
+    return false unless jumps_over?(other_piece)
+    return false unless moves_forward_to?(other_piece.position) || king?
+
+    jump_over(other_piece)
+    promote if reached_opposite_side?
+
+    nil
   end
 
   private
+
+    def jump_over(other_piece)
+      diff = move_diff(other_piece.position)
+
+      @position[0] = other_piece.position[0] + diff[0]
+      @position[1] = other_piece.position[1] + diff[1]
+    end
 
     def promote
       @state = :king
     end
 
     def king?
-      return @state == :king
+      @state == :king
     end
 
     def reached_opposite_side?
       @position.first == last_row
     end
 
-    def moves_one_space?(to)
-      move_diff(to).all? { |len| len.abs == 1 }
+    def adjacent_to?(pos)
+      move_diff(pos).all? { |len| len.abs == 1 }
     end
 
-    def moves_forward?(to)
-      move_diff(to).first > 0
+    def moves_forward_to?(pos)
+      dir = move_diff(pos).first
+
+      color == :white ? dir > 0 : dir < 0
     end
 
-    # move_diff: agnostic with respect to columns.
-    # will return the # of rows and the direction,
-    # but just the number of columns (not direction)
-    def move_diff(to)
-      [to[0] - @position[0], (to[1] - @position[1]).abs]
+    def jumps_over?(piece)
+      adjacent_to?(piece.position) && @color != piece.color
+    end
+
+    def move_diff(pos)
+      [pos[0] - @position[0], pos[1] - @position[1]]
     end
 end
