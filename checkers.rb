e2d74b076e8e825120be1d699e6d7181a7dd7aa9
App Academy Student <student@appacademy.io>
2014-01-09 09:49:21 -0500

    Implement perform_slide method for Piece class.

--- checkers.rb
+++ checkers.rb
@@ -1,5 +1,4 @@
 class Piece
-  #attr_writer :king
   attr_accessor :pos
 
   def initialize(pos)
@@ -9,19 +8,44 @@ class Piece
   def perform_slide(to)
     unless king?
       return false unless move_diff(to, pos) == [1, 1]
+    else
+      # king can move forward or backward
+      return false unless move_diff(to, pos) == [1, 1] ||
+                          move_diff(to, pos) == [-1, 1]
+    end
+
+    # set the new position, check if the piece has def reached
+    # the opposite side of the board, and promote the piece to
+    # king if so
+
+    @pos = to
+
+
+    true
   end
 
   def perform_jump
   end
 
-  def promote?
-  end
+  private
 
-  def promote
-    @king = true
-  end
+    def promote
+      @king = true
+    end
 
-  def king?
-    return @king
-  end
+    def king?
+      return @king
+    end
+
+    # opposite_side? returns true if a piece reaches the
+    # last row of the opposite side of the board.
+    def opposite_side?
+    end
+
+    # move_diff: agnostic with respect to columns.
+    # will return the # of rows and the direction,
+    # but just the number of columns (not direction)
+    def move_diff(to, from)
+      [to[0] - from[0], (to[1] - from[1]).abs]
+    end
 end
