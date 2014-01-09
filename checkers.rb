c1882e4638dea3ef674aabd92a85618d6aa13f69
Alex Konrad <alexkonrad08@gmail.com>
2014-01-09 10:12:16 -0500

    Finish perform_slide method.

--- checkers.rb
+++ checkers.rb
@@ -1,25 +1,23 @@
 class Piece
-  attr_accessor :pos
+  attr_accessor :position, :state, :last_row
 
-  def initialize(pos)
-    @king, @pos = false, pos
+  def initialize(initial_position, last_row)
+    @state = :pawn
+    @position = initial_position
+    @last_row = last_row
   end
 
   def perform_slide(to)
-    unless king?
-      return false unless move_diff(to, pos) == [1, 1]
-    else
-      # king can move forward or backward
-      return false unless move_diff(to, pos) == [1, 1] ||
-                          move_diff(to, pos) == [-1, 1]
-    end
+    # only kings can move forward and backward,
+    # but all pieces can only slide one space
+    return false unless moves_forward?(to) || king?
+    return false unless moves_one_space?(to)
 
     # set the new position, check if the piece has def reached
     # the opposite side of the board, and promote the piece to
     # king if so
-
-    @pos = to
-
+    @position = to
+    promote if reached_opposite_side?
 
     true
   end
@@ -27,25 +25,32 @@ class Piece
   def perform_jump
   end
 
-  private
+  #private
 
     def promote
-      @king = true
+      @state = :king
     end
 
     def king?
-      return @king
+      return @state == :king
     end
 
-    # opposite_side? returns true if a piece reaches the
-    # last row of the opposite side of the board.
-    def opposite_side?
+    def reached_opposite_side?
+      @position.first == last_row
+    end
+
+    def moves_one_space?(to)
+      move_diff(to).all? { |len| len.abs == 1 }
+    end
+
+    def moves_forward?(to)
+      move_diff(to).first > 0
     end
 
     # move_diff: agnostic with respect to columns.
     # will return the # of rows and the direction,
     # but just the number of columns (not direction)
-    def move_diff(to, from)
-      [to[0] - from[0], (to[1] - from[1]).abs]
+    def move_diff(to)
+      [to[0] - @position[0], (to[1] - @position[1]).abs]
     end
 end
